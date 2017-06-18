5bacae1ff3be88f0fb00e42c4d31bf4485386898
kidsana <kidsana.p@gmail.com>
2017-06-18 15:16:56 +0700

    กำหนด Template

--- application/config/routes.php
+++ application/config/routes.php
@@ -53,6 +53,7 @@ $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = TRUE;
 
+//$route['main/login'] = 'main/login';
 /*
 | -------------------------------------------------------------------------
 | Sample REST API Routes
