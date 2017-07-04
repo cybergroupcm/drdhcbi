fa804200a24b9f226c65f0ac149faa6e99d74de8
ZenTBelL <peerasak.sane@hotmail.com>
2017-07-04 21:33:20 +0700

    fix deploy code

--- assets/js/complaint_dashboard.js
+++ assets/js/complaint_dashboard.js
@@ -1,9 +1,7 @@
 $( document ).ready(function() {
     $('.datepicker').datepicker({
-        format: 'dd/mm/yyyy',
-        todayBtn: true,
-        language: 'th',             //เปลี่ยน label ต่างของ ปฏิทิน ให้เป็น ภาษาไทย   (ต้องใช้ไฟล์ bootstrap-datepicker.th.min.js นี้ด้วย)
-        thaiyear: true              //Set เป็นปี พ.ศ.
+        language: 'th',
+        thaiyear: true
     }).datepicker("setDate", "0");  //กำหนดเป็นวันปัจุบัน
 
     $('#bt_add').click(function(){
--- assets/js/key_in.js
+++ assets/js/key_in.js
@@ -173,7 +173,8 @@ function get_subdistrict(value,defaule_value){
 
 $(document).ready(function () {
     $('.datepicker').datepicker({
-        language: 'th',
+        language: 'th-th',
+        format: 'dd/mm/yyyy',
         thaiyear: true
     });
     changeUserComplain();
