959c8b17ece417f80169711ca24caa5d7ad76d6d
bloodyglove <ts_wis@hotmail.com>
2017-07-12 23:19:57 +0700

    การแสดงผล รายการเรื่องร้องเรียน ตามสิทธิ์การมองเห็นของ user

--- application/controllers/api/Complaint.php
+++ application/controllers/api/Complaint.php
@@ -19,8 +19,15 @@ class Complaint extends REST_Controller
     public function dashboard_get()
     {
         $page = $this->get('page');
-        $total_complaint = $this->Key_in_model->count_rows(); // retrieve the total number of posts
-        $complaint = $this->Key_in_model->order_by('keyin_id', 'DESC')->with_title_name('fields:prename')->with_complaint_type('fields:complain_type_name')->with_wish('fields:wish_name')->with_current_status('fields:current_status_name')->paginate(15, $total_complaint, $page); // paginate with 10 rows per page -
+        $overall = $this->get('overall');
+        $user_id = $this->get('user_id');
+        if( $overall == 1 ) { // มองเห็น เรื่องร้องเรียนทั้งหมด
+            $total_complaint = $this->Key_in_model->count_rows(); // retrieve the total number of posts
+            $complaint = $this->Key_in_model->order_by('keyin_id', 'DESC')->with_title_name('fields:prename')->with_complaint_type('fields:complain_type_name')->with_wish('fields:wish_name')->with_current_status('fields:current_status_name')->paginate(15, $total_complaint, $page); // paginate with 10 rows per page -
+        }else{
+            $total_complaint = $this->Key_in_model->where('create_user_id', $user_id)->count_rows(); // retrieve the total number of posts
+            $complaint = $this->Key_in_model->where('create_user_id', $user_id)->order_by('keyin_id', 'DESC')->with_title_name('fields:prename')->with_complaint_type('fields:complain_type_name')->with_wish('fields:wish_name')->with_current_status('fields:current_status_name')->paginate(15, $total_complaint, $page); // paginate with 10 rows per page -
+        }
 
         if ($complaint) {
             // Set the response and exit
@@ -37,8 +44,13 @@ class Complaint extends REST_Controller
 
     public function total_row_get()
     {
-        $total_complaint = $this->Key_in_model->count_rows(); // retrieve the total number of posts
-
+        $overall = $this->get('overall');
+        $user_id = $this->get('user_id');
+        if( $overall == 1 ) { // มองเห็น เรื่องร้องเรียนทั้งหมด
+            $total_complaint = $this->Key_in_model->count_rows(); // retrieve the total number of posts
+        }else{
+            $total_complaint = $this->Key_in_model->where('create_user_id', $user_id)->count_rows();
+        }
         if ($total_complaint) {
             // Set the response and exit
             $this->response($total_complaint, REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
