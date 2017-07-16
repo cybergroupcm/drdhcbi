b6f661ca5c31995aa84a20d3e220bf1601f78ede
pipeanny <bustercall_pipe@hotmail.com>
2017-07-16 15:40:42 +0700

    รายงาน report_all_complaint

--- application/controllers/Report.php
+++ application/controllers/Report.php
@@ -8,8 +8,8 @@ class Report extends CI_Controller {
 
         /* Load :: Common */
         //$this->load->helper('cookie');
-        //$this->load->helper('api');
         $this->load->helper('form');
+        $this->load->helper(array('html', 'url', 'api'));
         //$this->load->model('complaint_model');
         $this->load->helper('dateformat');
         $this->load->library('my_mpdf');
@@ -17,11 +17,54 @@ class Report extends CI_Controller {
 
     public function report_all_complaint()
     {
-        $url = base_url('api/complaint/complaint_type');
+        $url = base_url('api/dropdown/complain_type_lists');
         $arr_data['complaint_type'] = api_call_get($url);
-        $url = base_url('api/complaint/channel');
+        $url = base_url('api/dropdown/channel_lists');
         $arr_data['channel'] = api_call_get($url);
+
+        $url = base_url("api/dropdown/ccaa_lists/Changwat");
+        $arr_data['province_list'] = api_call_get($url);
+
+        if(@$province_id!=''){
+            $ccaa_code = substr(@$province_id, 0, 3);
+            $url = base_url("api/dropdown/ccaa_lists/Aumpur/".$ccaa_code);
+            $arr_data['district_list'] = api_call_get($url);
+        }
+
+
+        if(@$district_id!=''){
+            $ccaa_code = substr(@$district_id, 0, 4);
+            $url = base_url("api/dropdown/ccaa_lists/Tamboon/".$ccaa_code);
+            $arr_data['subdistrict_list'] = api_call_get($url);
+        }
+
+        $url = base_url("api/report/report_all_complaint");
+        $arr_data['data'] = api_call_get($url);
+        //echo"<pre>";print_r($arr_data['data']);echo"</pre>";
         $this->libraries->template('report_all_complaint/report_all_complaint', $arr_data);
+    }
+
+    public function report_all_complaint_pdf()
+    {
+        $url = base_url('api/dropdown/complain_type_lists');
+        $arr_data['complaint_type'] = api_call_get($url);
+        $url = base_url('api/dropdown/channel_lists');
+        $arr_data['channel'] = api_call_get($url);
+        $url = base_url()."api/report/report_all_complaint";
+        $arr_data['data'] = api_call_get($url);
+        $html=$this->load->view('report_all_complaint/report_all_complaint_pdf',$arr_data, true);
+        // As PDF creation takes a bit of memory, we're saving the created file in /downloads/reports/
+//        echo "<pre>";
+//        print_r($this->my_mpdf);
+//        die();
+        $mpdf=new mPDF('th','A4-L',0,'THSaraban',15,15,16,16,9,9, 'L');
+        $mpdf->SetDisplayMode('fullpage');
+        $mpdf->list_indent_first_level = 0;
+        //$stylesheet = file_get_contents(APPPATH.'third_party/mpdf/css/mpdfstyletables.css');
+        //$this->mpdf->WriteHTML($stylesheet, 1);
+        $mpdf->WriteHTML($html, 2);
+        $mpdf->Output('example_mpdf.pdf', 'I');
+        exit;
     }
     
     public function report_by_channel()
@@ -51,10 +94,17 @@ class Report extends CI_Controller {
         $this->libraries->template('report_by_complainant/report_by_complainant', $arr_data);
     }
     
-    public function report_statistic_by_type()
+    public function report_statistic_by_type($year='')
     {
-        $url = base_url('api/complaint/complaint_type');
-        $arr_data['complaint_type'] = api_call_get($url);
+
+        $url = base_url("api/dropdown/complain_type_lists");
+        $arr_data['complain_type'] = api_call_get($url);
+
+        $url = base_url()."api/report/month_report/".$year;
+        $arr_data['month_report'] = api_call_get($url);
+
+        $url = base_url()."api/report/report_statistic_by_type/".$year;
+        $arr_data['report_type'] = api_call_get($url);
         $this->libraries->template('report_statistic_by_type/report_statistic_by_type', $arr_data);
     }
     
--- application/views/report_all_complaint/report_all_complaint.php
+++ application/views/report_all_complaint/report_all_complaint.php
@@ -19,12 +19,15 @@ $link = array(
     'type' => 'text/javascript'
 );
     echo script_tag($link);
-$link = array(
-    'src' => 'assets/js/report.js',
-    'type' => 'text/javascript'
-);
-    echo script_tag($link);
 $this->load->view('report_all_complaint/search');
+
+function replace_empty($value){
+    if($value==''){
+        return '0';
+    }else{
+        return $value;
+    }
+}
 ?>
 <section class="content">
     <div class="row">
@@ -35,35 +38,40 @@ $this->load->view('report_all_complaint/search');
                 </div>
                 <div class="box-body">
                     <div class="col-xs-12 text-right" style="margin-bottom: 5px;">
-                        <?php
-                        echo img(array('src'=>'assets/images/search.png', 'title'=> 'ค้นหาข้อมูล','width'=>'48px','style'=>'cursor:pointer','data-toggle'=>'modal','data-target'=>'#search'));
-                        echo img(array('src'=>'assets/images/print.png', 'title'=> 'สั่งพิมพ์','width'=>'48px','style'=>'cursor:pointer'));
-                        ?>
+        <?php echo img(array('src'=>'assets/images/search.png', 'title'=> 'ค้นหาข้อมูล','width'=>'48px','style'=>'cursor:pointer','data-toggle'=>'modal','data-target'=>'#search')); ?>
+        <a href="<?php echo base_url("report/report_all_complaint_pdf");?>" target="_blank"><?php echo img(array('src'=>'assets/images/print.png', 'title'=> 'สั่งพิมพ์','width'=>'48px','style'=>'cursor:pointer')); ?></a>
                     </div>
-                    <div class="col-xs-12" id="donut-chart" style="height: 200px;"></div>
+                    <div class="col-xs-7" id="donut-chart" style="height: 200px;"></div>
+                    <div class="col-xs-5 text-right">
+                        <?php echo img(array('src'=>'assets/images/pic_right.jpg','width'=>'450px','style'=>'margin-right: -15px;opacity: 0.6;'));?>
+                    </div>
                     <table id="example1" class="table table-bordered table-striped table-hover dataTable">
                         <thead>
                         <tr>
                             <th class="text-center" rowspan="2">ประเภทการร้องทุกข์</th>
-                            <th class="text-center" colspan="12">ช่องทางการร้องทุกข์</th>
+                            <th class="text-center" colspan="<?php echo count($channel);?>">ช่องทางการร้องทุกข์</th>
+                            <th class="text-center" rowspan="2">รวม</th>
                         </tr>
                         <tr>
-                            <th class="text-center">มาด้วยตนเอง</th>
-                            <th class="text-center">โทรศัพท์/โทรสาร/ฝากข้อความอัตโนมัติ 1567</th>
-                            <th class="text-center">สำนักนายกรัฐมนตรี 1111</th>
-                            <th class="text-center">จดหมาย</th>
-                            <th class="text-center">สำนักงานรัฐมนตรีกระทรวงมหาดไทย</th>
-                            <th class="text-center">ศูนย์ดำรงธรรมจังหวัด</th>
-                            <th class="text-center">ปลัดกระทรวงมหาดไทย</th>
-                            <th class="text-center">กระทรวง/กรม/อื่นๆ</th>
-                            <th class="text-center">ผ่าน สนง.ผู้ตรวจการแผ่นดิน</th>
-                            <th class="text-center">ศูนย์ดำรงธรรมอำเภอ</th>
-                            <th class="text-center">ศูนย์ดำรงธรรมส่วนกลาง</th>
-                            <th class="text-center">รวม</th>
+                            <?php foreach($channel as $key => $value){ ?>
+                            <th class="text-center"><?php echo $value; ?></th>
+                            <?php } ?>
+
                         </tr>
                         </thead>
                         <tbody>
-                        
+                        <?php foreach($complaint_type as $key => $value){ ?>
+                            <tr>
+                                <td><?php echo $value; ?></td>
+                                <?php foreach($channel as $key2 => $value2){ ?>
+                                    <td align="right"><?php echo replace_empty(@$data[$key][$key2]); ?></td>
+                                <?php
+                                    @$data[$key]['sum_all'] += replace_empty(@$data[$key][$key2]);
+                                } ?>
+                                <td align="right"><?php echo replace_empty(@$data[$key]['sum_all']); ?></td>
+                            </tr>
+                        <?php } ?>
+
                         </tbody>
                     </table>
                     <?php //echo $pagination; ?>
@@ -73,43 +81,54 @@ $this->load->view('report_all_complaint/search');
     </div>
 </section>
 <div id="base_url" class="<?php echo base_url();?>"></div>
+<?php
+$link = array(
+    'src' => 'assets/js/report.js',
+    'type' => 'text/javascript'
+);
+echo script_tag($link);
+?>
+<?php
+$i=1;
+$letter = array();
+for($a='a';$a<'z';$a++)
+{
+    $letter[$i] = $a;
+    $i++;
+}
+$chart_data='';
+
+    foreach($complaint_type as $key => $value){
+        $j=1;
+        $chart_data .= "{ y: '".$value."', ";
+        $labels = '[';
+        $ykeys = '[';
+        foreach($channel as $key2 => $value2){
+            $chart_data .= $letter[$j].": ".replace_empty(@$data[$key][$key2]).", ";
+            $labels .= "'".$value2."', ";
+            $ykeys .= "'".$letter[$j]."', ";
+            $j++;
+        }
+        $chart_data .= " },";
+        $labels = substr($labels, 0, -1);
+        $labels .= ']';
+        $ykeys = substr($ykeys, 0, -1);
+        $ykeys .= ']';
+    }
+$chart_data = substr($chart_data, 0, -1);
+?>
 <script>
-/*
-         * DONUT CHART
-         * -----------
-         */
-
-        var donutData = [
-          {label: "Series2", data: 30, color: "#3c8dbc"},
-          {label: "Series3", data: 20, color: "#0073b7"},
-          {label: "Series4", data: 50, color: "#00c0ef"}
-        ];
-        $.plot("#donut-chart", donutData, {
-          series: {
-            pie: {
-              show: true,
-              radius: 1,
-              innerRadius: 0.5,
-              label: {
-                show: true,
-                radius: 2 / 3,
-                formatter: labelFormatter,
-                threshold: 0.1
-              }
-
-            }
-          },
-          legend: {
-            show: false
-          }
-        });
-        /*
-         * END DONUT CHART
-         */
-        /*
-       * Custom Label formatter
-       * ----------------------
-       */
+    //BAR CHART
+    var bar = new Morris.Bar({
+        element: 'donut-chart',
+        resize: true,
+        data: [<?php echo $chart_data; ?>],
+        barColors: ['#3c8dbc', '#0073b7', '#00c0ef'],
+        xkey: 'y',
+        ykeys: <?php echo $ykeys; ?>,
+        labels: <?php echo $labels; ?>,
+        hideHover: 'auto'
+    });
       function labelFormatter(label, series) {
         return "<div style='font-size:13px; text-align:center; padding:2px; color: #fff; font-weight: 600;'>"
                 + label
