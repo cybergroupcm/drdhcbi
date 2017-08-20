2696f69bc354e0dcd255d34aa81f41992368b041
bloodyglove <ts_wis@hotmail.com>
2017-08-20 15:34:04 +0700

    commit 20/08/2560

--- application/views/complaint/step_of_keyin.php
+++ application/views/complaint/step_of_keyin.php
@@ -6,33 +6,35 @@ $link = array(
 );
 echo link_tag($link);
 $step_array = array();
-$step_array[1] = 'บันทึกเรื่องร้องทุกข์/ร้องเรียน';
-$step_array[2] = 'เนื้อหาเรื่องร้องทุกข์ร้องเรียน';
-$step_array[3] = 'ความประสงค์ในการดำเนินการ';
-$step_array[4] = 'หลักฐานประกอบเรื่องร้องเรียน/ร้องทุกข์';
-$step_array[5] = 'สรุปข้อมูลเรื่องร้องเรียน/ร้องทุกข์';
+$step_array[1] = 'บันทึกเรื่องร้องทุกข์';
+$step_array[2] = 'เนื้อหา';
+$step_array[3] = 'ความประสงค์';
+$step_array[4] = 'หลักฐาน';
+$step_array[5] = 'สรุปข้อมูล';
+//echo $key_in_data['step'];
 ?>
-        <div class="stepwizard col-md-offset-0">
-            <div class="stepwizard-row setup-panel">
-                <?php foreach($step_array as $key => $value){
-                    if($step == $key){
-                        $btn = 'btn-primary';
-                    }else{
-                        $btn = 'btn-default';
-                    }
-                    if($key <= $key_in_data['step']+1){
-                        $disabled = '';
-                    }else{
-                        $disabled = 'disabled="disabled"';
+
+    <div class="container">
+        <div class="row">
+            <ul class="breadcrumb">
+                <?php foreach($step_array as $key => $value) {
+                    if ($key > $step ) {
+                        $class = '';
+                    }elseif( $step == $key ){
+                        $class = 'active';
+                    } else {
+                        $class = 'completed';
                     }
                     ?>
-                <div class="stepwizard-step">
-                        <a type="button" class="btn <?php echo $btn; ?> btn-circle" <?php echo $disabled; ?> onclick="location.href='<?php echo base_url('complaint/key_in/key_in_step'.$key.'/'.@$id); ?>'"><?php echo $key; ?></a>
-                        <p><?php echo $value; ?></p>
-                </div>
-                <?php } ?>
-            </div>
+                    <li class="<?php echo $class;?>">
+                        <a style="cursor: pointer;" onclick="location.href='<?php echo base_url('complaint/key_in/key_in_step' . $key . '/' . @$id); ?>'"><?php echo $step_array[$key]; ?></a>
+                    </li>
+                    <?php
+                        }
+                    ?>
+            </ul>
         </div>
+    </div>
 <?php
 $link = array(
     'src' => 'assets/js/step.js',
