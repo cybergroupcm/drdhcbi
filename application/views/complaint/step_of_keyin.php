277fcf27c827efc3143c2eae40ea124bd5205e25
bloodyglove <ts_wis@hotmail.com>
2017-08-22 22:13:22 +0700

    commit 22/08/2560

--- application/views/complaint/step_of_keyin.php
+++ application/views/complaint/step_of_keyin.php
@@ -14,25 +14,27 @@ $step_array[5] = 'สรุปข้อมูล';
 //echo $key_in_data['step'];
 ?>
 
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
-            <ul class="breadcrumb">
-                <?php foreach($step_array as $key => $value) {
-                    if ($key > $step ) {
-                        $class = '';
-                    }elseif( $step == $key ){
-                        $class = 'active';
-                    } else {
-                        $class = 'completed';
-                    }
-                    ?>
-                    <li class="<?php echo $class;?>">
-                        <a style="cursor: pointer;" onclick="location.href='<?php echo base_url('complaint/key_in/key_in_step' . $key . '/' . @$id); ?>'"><?php echo $step_array[$key]; ?></a>
-                    </li>
-                    <?php
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <ul class="breadcrumb">
+                    <?php foreach($step_array as $key => $value) {
+                        if ($key > $step ) {
+                            $class = '';
+                        }elseif( $step == $key ){
+                            $class = 'active';
+                        } else {
+                            $class = 'completed';
                         }
-                    ?>
-            </ul>
+                        ?>
+                        <li class="<?php echo $class;?>">
+                            <a style="cursor: pointer;" onclick="location.href='<?php echo base_url('complaint/key_in/key_in_step' . $key . '/' . @$id); ?>'"><?php echo $step_array[$key]; ?></a>
+                        </li>
+                        <?php
+                            }
+                        ?>
+                </ul>
+            </div>
         </div>
     </div>
 <?php
