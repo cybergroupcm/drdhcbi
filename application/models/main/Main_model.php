46fd73a7ca5e5950759bb34ef60e0bb3d4c37912
taeman <taemanster@gmail.com>
2018-01-19 20:32:08 +0700

    เพิ่มเงื่อนไข ไม่ให้กราฟแสดงรายการที่สถานะเป็นยกเลิก

--- application/models/main/Main_model.php
+++ application/models/main/Main_model.php
@@ -8,16 +8,19 @@ class Main_model extends CI_Model {
         parent::__construct();
     }
 
-    public function get_sum_status($user_id='')
+    public function get_sum_status($user_id='',$year=null)
     {
       $where = "";
       if( $user_id != '' ){
-            $where = " WHERE report_statistic_by_status.create_user_id = '".$user_id."' ";
+            $where .= " AND report_statistic_by_status.create_user_id = '".$user_id."' ";
+      }
+      if(!is_null($year)&&!empty($year)){
+            $where .= " AND report_statistic_by_status.complain_year = '".$year."' ";
       }
       $sql = "  SELECT SUM(report_statistic_by_status.sum_complain) AS sum_complain,
                       report_statistic_by_status.current_status_id
                 FROM report_statistic_by_status
-                ".$where."
+                WHERE 1=1 ".$where."
                 GROUP BY report_statistic_by_status.current_status_id
                 ORDER BY report_statistic_by_status.current_status_id ASC ";
       $query = $this->db->query($sql);
@@ -35,7 +38,18 @@ class Main_model extends CI_Model {
       return $result;
     }
 
-    public function get_sum_status_without_ccaa($user_id='')
+    public function get_min_year()
+    {
+        $sql = "SELECT Min(YEAR(complain_date)) AS min_year FROM dt_keyin";
+        $query = $this->db->query($sql);
+        $row = $query->row();
+        if (isset($row)) {
+            return $row->min_year;
+        }
+        return '';
+    }
+
+    public function get_sum_status_without_ccaa($user_id='',$year)
     {
 
         $where = "";
@@ -60,8 +74,15 @@ class Main_model extends CI_Model {
             }
             $company_in =implode(',', $company);
             if(!empty($data_group)){
-                $where_user = " OR (`dt_keyin`.send_org_id IN (".$company_in."))";
+                $where_user .= " OR (`dt_keyin`.send_org_id = '".$data_group[0]['company']."')";
             }
+
+            if(!empty($company)){
+                $where_user .= " OR (`dt_keyin`.send_org_id IN (".$company_in."))";
+            }
+        }
+        if(!is_null($year)&&!empty($year)){
+            $where .= " AND YEAR(complain_date) = '".$year."' ";
         }
         $sql = "SELECT
                     IF(`dt_keyin`.`current_status_id`>=4,4,`dt_keyin`.`current_status_id`) AS `current_status_id`,
@@ -108,9 +129,51 @@ class Main_model extends CI_Model {
       return $result;
     }
 
-    public function get_data_status($status_id){
-        $to_day = date('Y-m-d');
-        //$to_day = '2017-10-10';
+    public function get_data_status($status_id,$user_id='',$year=''){
+       // $to_day = date('Y-m-d');
+       // $to_day = '2017-10-25';
+        if(empty($year) || is_null($year)){
+            $to_day = date('Y-m-d');
+            $where_year = " AND complain_date LIKE('".$to_day."%') ";
+        }else{
+            $where_year = " AND YEAR(complain_date) = '".$year."' ";;
+        }
+        $where = "";
+        $where_user = "";
+        if( $user_id != '' ) {
+
+            $sql = "SELECT
+                  `au_users`.company,
+                  `au_users_groups`.group_id
+                FROM `au_users` INNER JOIN `au_users_groups` ON `au_users`.id = `au_users_groups`.user_id
+                WHERE `au_users`.id = '".$user_id."'
+                ";
+            $query = $this->db->query($sql);
+            $data_group = $query->result_array();
+
+            $sql_send_org = "SELECT send_org_id FROM ms_send_org WHERE  parent_id = '".$data_group[0]['company']."'";
+            $query_send_org = $this->db->query($sql_send_org);
+            $data_send_org = $query_send_org->result_array();
+            $company = array();
+            foreach ($data_send_org as $item => $value){
+                $company[$item] = $value['send_org_id'];
+            }
+            $company_in =implode(',', $company);
+
+            if(!empty($data_group)){
+                $where_user .= " OR (`dt_keyin`.send_org_id = '".$data_group[0]['company']."')";
+            }
+
+            if(!empty($company)){
+                $where_user .= " OR (`dt_keyin`.send_org_id IN (".$company_in."))";
+            }
+
+            if($where_user != ""){
+                $where = " AND (dt_keyin.create_user_id = '" . $user_id . "' $where_user ) ";
+            }else{
+                $where = " AND (dt_keyin.create_user_id = '" . $user_id . "') ";
+            }
+        }
         $sql = "SELECT
                 dt_keyin.keyin_id,
                 dt_keyin.complain_no,
@@ -123,7 +186,8 @@ class Main_model extends CI_Model {
                 ON dt_keyin.complain_type_id = ms_complain_type.complain_type_id
                 WHERE ms_complain_type.parent_id != 0
                 AND current_status_id='".$status_id."'
-                AND complain_date LIKE('".$to_day."%')
+                ".$where_year."
+                ".$where."
                 ";
         $query = $this->db->query($sql);
         $result = $query->result();
@@ -187,25 +251,83 @@ class Main_model extends CI_Model {
       return $result;
     }
 
-    public function get_sum_type($user_id='')
+    public function get_sum_type($user_id='',$year='')
     {
-        $to_day = date('Y-m-d');
-        //$to_day = '2017-09-27';
-        $where = "";
-        if( $user_id != '' ){
-            $where = "  WHERE report_all_complaint.create_user_id = '".$user_id."'
-                        AND report_all_complaint.complain_date LIKE('".$to_day."%') ";
+        if(empty($year) || is_null($year)){
+            $to_day = date('Y-m-d');
+            $where_year = " AND complain_date LIKE('".$to_day."%') ";
         }else{
-            $where = "  WHERE report_all_complaint.complain_date LIKE('".$to_day."%') ";
+            $where_year = " AND YEAR(complain_date) = '".$year."' ";;
         }
-      $sql = " SELECT report_all_complaint.complain_type_id,
-              	ms_complain_type.complain_type_name,
-              	ms_complain_type.parent_id,
-              	SUM(report_all_complaint.sum_complain) AS sum_complain
-              FROM report_all_complaint INNER JOIN ms_complain_type ON report_all_complaint.complain_type_id = ms_complain_type.complain_type_id
-              ".$where."
-              GROUP BY report_all_complaint.complain_type_id
-              ORDER BY sum_complain DESC";
+
+        //$to_day = '2017-10-27';
+        //$where = "";
+//        if( $user_id != '' ){
+//            $where = "  WHERE report_all_complaint.create_user_id = '".$user_id."'
+//                        AND report_all_complaint.complain_date LIKE('".$to_day."%') ";
+//        }else{
+//            $where = "  WHERE report_all_complaint.complain_date LIKE('".$to_day."%') ";
+//        }
+//      $sql = " SELECT report_all_complaint.complain_type_id,
+//              	ms_complain_type.complain_type_name,
+//              	ms_complain_type.parent_id,
+//              	SUM(report_all_complaint.sum_complain) AS sum_complain
+//              FROM report_all_complaint INNER JOIN ms_complain_type ON report_all_complaint.complain_type_id = ms_complain_type.complain_type_id
+//              ".$where."
+//              GROUP BY report_all_complaint.complain_type_id
+//              ORDER BY sum_complain DESC";
+        $where = "";
+        $where_user = "";
+        if( $user_id != '' ) {
+
+            $sql = "SELECT
+                  `au_users`.company,
+                  `au_users_groups`.group_id
+                FROM `au_users` INNER JOIN `au_users_groups` ON `au_users`.id = `au_users_groups`.user_id
+                WHERE `au_users`.id = '".$user_id."'
+                ";
+            $query = $this->db->query($sql);
+            $data_group = $query->result_array();
+
+            $sql_send_org = "SELECT send_org_id FROM ms_send_org WHERE  parent_id = '".$data_group[0]['company']."'";
+            $query_send_org = $this->db->query($sql_send_org);
+            $data_send_org = $query_send_org->result_array();
+            $company = array();
+            foreach ($data_send_org as $item => $value){
+                $company[$item] = $value['send_org_id'];
+            }
+            $company_in =implode(',', $company);
+
+            if(!empty($data_group)){
+                $where_user .= " OR (`dt_keyin`.send_org_id = '".$data_group[0]['company']."')";
+            }
+
+            if(!empty($company)){
+                $where_user .= " OR (`dt_keyin`.send_org_id IN (".$company_in."))";
+            }
+
+            if($where_user != ""){
+                $where = " AND (dt_keyin.create_user_id = '" . $user_id . "' $where_user ) ";
+            }else{
+                $where = " AND (dt_keyin.create_user_id = '" . $user_id . "') ";
+            }
+        }
+       $sql = "SELECT
+                    dt_keyin.complain_type_id,
+                    ms_complain_type.complain_type_name,
+                    ms_complain_type.parent_id,
+                    COUNT( dt_keyin.keyin_id ) AS sum_complain 
+                FROM
+                    dt_keyin
+                    INNER JOIN ms_complain_type ON dt_keyin.complain_type_id = ms_complain_type.complain_type_id 
+                WHERE 1 = 1
+                ".$where_year."
+                ".$where."
+                    AND dt_keyin.current_status_id <> '5'
+                GROUP BY
+                    dt_keyin.complain_type_id 
+                ORDER BY
+                    sum_complain DESC";
               //LIMIT 5 ";
       $query = $this->db->query($sql);
       $sum_all = 0;
